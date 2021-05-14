a2906aa42f13250f17f72f93e3f72362ba6f16a4
Adam La Morre <alamorre@gmail.com>
2021-05-13 19:38:08 -0700

    context

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from "react"
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
-// import { AuthProvider } from "../contexts/AuthContext"
+import { AuthProvider } from "../contexts/AuthContext"
 
 // import Chats from "./Chats"
 import Login from "./Login"
@@ -11,12 +11,12 @@ function App() {
   return (
     <div style={{ fontFamily: 'Avenir' }}>
       <Router>
-        {/* <AuthProvider> */}
+        <AuthProvider>
           <Switch>
             {/* <Route path="/chats" component={Chats} /> */}
             <Route path="/" component={Login} />
           </Switch>
-        {/* </AuthProvider> */}
+        </AuthProvider>
       </Router>
     </div>
   )
